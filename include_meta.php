fc40811e19c1605452dde5586d637c781aaebffe
AymanF <aymanfattar@gmail.com>
2018-05-01 20:10:52 +0200

    Correcrtion d'un bug css au niveau du meta. Css OP via Bootstrap

--- include_meta.php
+++ include_meta.php
@@ -1,23 +1,10 @@
 <?php
 
-echo "<meta charset=\"utf-8\">
-    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
-    <meta name=\"description\" content=\"\">
-    <meta name=\"author\" content=\"\">
-
-    <title>Students Log In</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href=\"vendor/bootstrap/css/bootstrap.min.css\" rel=\"stylesheet\">
-
-    <!-- Custom fonts for this template -->
-    <link href=\"vendor/font-awesome/css/font-awesome.min.css\" rel=\"stylesheet\" type=\"text/css\">
-    <link href=\"vendor/simple-line-icons/css/simple-line-icons.css\" rel=\"stylesheet\" type=\"text/css\">
-    <link href=\"https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic\" rel=\"stylesheet\" type=\"text/css\">
-
-    <!-- Custom styles for this template -->
-    <link href=\"css/landing-page.min.css\" rel=\"stylesheet\">
-    <link rel='stylesheet' type='text/css' href='css/LoginRegister.css'>
-    <link href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" rel=\"stylesheet\"/>
+echo "
+<link href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" rel=\"stylesheet\"/>
     <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js\"></script>
-    <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>";
+    <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"../Bootstrap/css/bootstrap.min.css\">
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"css/LoginRegister.css\">
+
+    <title>Students Log In</title>";
